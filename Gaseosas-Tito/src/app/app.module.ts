1715165abbdb2d15622fec1e09b1d13993f4e60a
Manuu1409 <manuracingclub61@gmail.com>
2022-09-20 20:06:42 -0300

    agregando bootstrap y creando componente

--- Gaseosas-Tito/src/app/app.module.ts
+++ Gaseosas-Tito/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaGaseosasComponent } from './lista-gaseosas/lista-gaseosas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaGaseosasComponent
   ],
   imports: [
     BrowserModule,
---
+++ Gaseosas-Tito/src/app/lista-gaseosas/lista-gaseosas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-gaseosas',
+  templateUrl: './lista-gaseosas.component.html',
+  styleUrls: ['./lista-gaseosas.component.scss']
+})
+export class ListaGaseosasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
